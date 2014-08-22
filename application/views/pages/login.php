a0af427b95f9ab84c8266145490459c9223ac919
carshelves <abdelselouane@gmail.com>
2014-08-22 14:51:23 -0400

    ftploy test001

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -15,7 +15,7 @@
                    
 					<div class="sell_box sell_box_1">
                         <h2><strong>Sign In</strong></h2>
-                        <!---->
+                        <!---->  
                          <form id="loginForm" action="<?= base_url().'login/signin';?>" method="POST" class="form-vertical" data-toggle="validator">
                                 <div class="form-group" style="width:500px;" >
                                     <? if(!empty($error) && $error === TRUE){?>
