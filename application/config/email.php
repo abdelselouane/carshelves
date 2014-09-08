a240c996c9b5a815f6d8f0308ab59aedf6213ff4
carshelves <abdelselouane@gmail.com>
2014-09-08 15:39:46 -0400

    email settings updtae 09/08/14

--- application/config/email.php
+++ application/config/email.php
@@ -10,9 +10,17 @@
 |	http://codeigniter.com/user_guide/libraries/email.html
 |
 */
-$config['mailtype'] = 'html';
-$config['charset'] = 'utf-8';
-$config['newline'] = "\r\n";
+$config['useragent']	= 'none';
+$config['protocol']		= 'smtp';
+$config['mailpath']		= 'none';
+$config['stmp_host']	= 'smtpout.secureserver.net';
+$config['stmp_user']	= 'office@carshelves.com';
+$config['stmp_pass']	= 'office1985004';
+$config['stmp_port']	= '25';
+$config['stmp_timeout'] = '60';
+$config['mailtype']		= 'html';
+$config['charset']		= 'utf-8';
+$config['newline']		= "\r\n";
 
 
 /* End of file email.php */
